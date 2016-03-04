8e92354235c172a573f9af074b654aeb43394ab7
James Harris <wopiancitizen123@gmail.com>
2016-03-04 01:47:14 +0000

    Fix card size

--- src/index.php
+++ src/index.php
@@ -39,15 +39,17 @@
       <div class="row">
 
         <div class="col s4">
-          <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Flag_of_Osaka_Prefecture.svg/1920px-Flag_of_Osaka_Prefecture.svg.png">
-          </div>
-          <div class="card-content">
-            <span class="card-title activator grey-text text-darken-4">Ōsaka<i class="material-icons right">more_vert</i></span>
-          </div>
-          <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">Ōsaka<i class="material-icons right">close</i></span>
-            <p>Information about Ōsaka's flag.</p>
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Flag_of_Osaka_Prefecture.svg/1920px-Flag_of_Osaka_Prefecture.svg.png">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Ōsaka<i class="material-icons right">more_vert</i></span>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Ōsaka<i class="material-icons right">close</i></span>
+              <p>Information about Ōsaka's flag.</p>
+            </div>
           </div>
         </div>
 
