6fd9b1a7e5c2ccf6ca1b3eb7f0d1e0c702cb244d
James Harris <wopiancitizen123@gmail.com>
2016-03-04 00:55:10 +0000

    Test with example data

--- src/index.php
+++ src/index.php
@@ -12,4 +12,42 @@
   </head>
 
   <body>
+
+    <div class="navbar-fixed">
+      <nav>
+        <div class="nav-wrapper">
+          <a href="/" class="brand-logo">ハタヤマ</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">National Flags</a></li>
+            <li><a href="#">Regional Flags</a></li>
+            <li><a href="#">Military Flags</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+    <div class="container">
+
+      <p class="flow-text">
+        The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs!
+      </p>
+
+      <div class="row">
+
+        <div class="col s4">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Flag_of_Osaka_Prefecture.svg/1920px-Flag_of_Osaka_Prefecture.svg.png">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Ōsaka<i class="material-icons right">more_vert</i></span>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Ōsaka<i class="material-icons right">close</i></span>
+            <p>Information about Ōsaka's flag.</p>
+          </div>
+        </div>
+
+      </div>
+
   </body>
+</html>
