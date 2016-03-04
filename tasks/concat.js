73f37bb8c7ef66ba4f375d13e7b0412141e038cc
James Harris <wopiancitizen123@gmail.com>
2016-03-04 00:58:36 +0000

    fix concat scheme

--- tasks/concat.js
+++ tasks/concat.js
@@ -5,10 +5,10 @@ module.exports = {
     dist: {
         files: {
             'dist/assets/js/app.js' : [
-                'src/assets/js/vendor/materialize.min.js',
+                'src/assets/javascript/vendor/materialize.min.js',
                 //'src/assets/js/vendor/instantclick.min.js',
                 //'src/assets/js/vendor/jquery.lazyload.min.js',
-                'src/assets/js/app.js'
+                'src/assets/javascript/app.js'
             ]
         }
     }
