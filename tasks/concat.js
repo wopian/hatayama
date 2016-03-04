98a1fde639a81cc8d422b7cd485f0e133a3490a0
James Harris <wopiancitizen123@gmail.com>
2016-03-04 00:56:31 +0000

    Ignore not-added JS libraries

--- tasks/concat.js
+++ tasks/concat.js
@@ -6,8 +6,8 @@ module.exports = {
         files: {
             'dist/assets/js/app.js' : [
                 'src/assets/js/vendor/materialize.min.js',
-                'src/assets/js/vendor/instantclick.min.js',
-                'src/assets/js/vendor/jquery.lazyload.min.js',
+                //'src/assets/js/vendor/instantclick.min.js',
+                //'src/assets/js/vendor/jquery.lazyload.min.js',
                 'src/assets/js/app.js'
             ]
         }
