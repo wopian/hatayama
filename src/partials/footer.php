05767963511e235563c5b031745725223738b5d0
James Harris <wopiancitizen123@gmail.com>
2016-03-17 16:28:28 +0000

    Smart language switcher

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -1,6 +1,7 @@
     </main>
 
     <footer class="page-footer">
+
       <div class="container">
         <div class="row">
           <div class="col 16 s12">
@@ -9,15 +10,18 @@
           </div>
         </div>
       </div>
+
       <div class="footer-copyright">
         <div class="container">
           Made with love by <a href="//github.com/wopian">wopian</a>
           <div class="right">
-            <a class="grey-text text-lighten-4 waves-effect btn-flat" href="/en/" >English</a>
-            <a class="grey-text text-lighten-4 waves-effect btn-flat" href="/ja/">Japanese</a>
+            <? include("partials/langSwitcher.php"); ?>
+            <a class="grey-text text-lighten-4 waves-effect btn-flat" href="<?=langSwitcher('en')?>" >English</a>
+            <a class="grey-text text-lighten-4 waves-effect btn-flat" href="<?=langSwitcher('ja')?>">Japanese</a>
           </div>
         </div>
       </div>
+
     </footer>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" type="text/javascript" data-no-instant></script>
---
+++ src/partials/langSwitcher.php
@@ -0,0 +1,7 @@
+<?
+  function langSwitcher ($buttonLang) {
+    $url = $_SERVER['REQUEST_URI'];
+    $urlPath = preg_replace('/^(en|ja)/'. '', $url);
+
+    return '/' . $buttonLang . $urlPath;
+  }
