132b1921d5acac13475280a9d8219d3892621cfb
James Harris <wopiancitizen123@gmail.com>
2016-03-17 16:49:36 +0000

    Update

--- src/views/nation.php
+++ src/views/nation.php
@@ -9,7 +9,7 @@
         <span class="card-title activator grey-text text-darken-4"><? $L(">osaka") ?><i class="material-icons right">keyboard_arrow_up</i></span>
       </div>
       <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4"><? $L(">osaka") ?><i class="material-icons right">close</i></span>
+        <span class="card-title grey-text text-darken-4">Osaka<i class="material-icons right">close</i></span>
         <p>Meaning of Ōsaka's flag:</p>
         <p>The blue stands for cleanness, freshness and intelligence and also represents the sky and sea due to Ōsaka City having both an airport and seaport. The blue also represents Ōsaka's nickname water city, due to having many rivers and facing two seas. The mon represents calabash, the symbol of Toyotomi Hideyoshi. Circles also mean the letter O.</p>
         <p><a href="#">More Information</a></p>
