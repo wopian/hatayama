55dcdea39769b9744c1b3c67512818607eba806c
James Harris <wopiancitizen123@gmail.com>
2016-05-06 15:40:20 +0100

    Update en.php

--- src/languages/en.php
+++ src/languages/en.php
@@ -1,7 +1,16 @@
 <?
   define("INTRODUCTION", "Hello world.");
+  
+  # Navigation text
   define("NAV_NATION", "National");
   define("NAV_PREFECTURE", "Prefecture");
   define("NAV_MUNICIPAL", "Municipal");
   define("NAV_MILITARY", "Military");
   define("NAV_MISC", "Misc");
+  
+  # Abbreviations for mobile users
+  define("NAV_NATION_ABBR", "Nat'l");
+  define("NAV_PREFECTURE_ABBR", "Pref");
+  define("NAV_MUNICIPAL_ABBR", "Muni");
+  define("NAV_MILITARY_ABBR", "Military");
+  
