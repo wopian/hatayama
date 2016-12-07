cb16ebdbb65d66a83d7d15e21ba19aea5a073d08
James Harris <wopiancitizen123@gmail.com>
2016-12-07 22:21:37 +0000

    Add unit tests for later

---
+++ test/spec/example.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var client = require('webdriverio').remote({
+    logLevel: process.env.TRAVIS ? 'command' : 'silent',
+    desiredCapabilities: {
+        browserName: 'phantomjs'
+    }
+}).init();
+
+describe('example', function() {
+    before(function(done) {
+        client.url('http://localhost:9000/index.html', done);
+    });
+
+    it('tests a feature', function(done) {
+        client
+            .getTitle(function(err, title) {
+                assert.equal(title, 'Example');
+            })
+            .call(done);
+    });
+
+    after(function(done) {
+        client.end(done);
+    });
+});
