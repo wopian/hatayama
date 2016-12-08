0d104856c4ae7940370cc43112a64fe4bfcc392c
James Harris <wopiancitizen123@gmail.com>
2016-12-08 15:13:48 +0000

    Add DOM tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,33 @@
 module.exports = function(config) {
     config.set({
-        frameworks: ['browserify', 'jasmine'],
+        basePath: './',
+        frameworks: ['browserify', 'jasmine-jquery', 'jasmine'],
+        colors: true,
         reporters: ['spec'],
         browsers: ['PhantomJS'],
         preprocessors: {
             'tests/**/*.js': ['browserify']
         },
         browserify: {
-            debug: true
-            //transform: ['brfs']
+            debug: true,
+            transform: ['brfs']
         },
         plugins: [
             'karma-jasmine',
+            'karma-jasmine-jquery',
             'karma-phantomjs-launcher',
             'karma-spec-reporter',
-            'karma-browserify'
+            'karma-browserify',
+            'brfs'
         ],
         files: [
-            'tests/**/*.js'
+            'tests/**/*.js', 'app/vendor/jquery-2.2.0.min.js',
+            {
+                pattern: 'tests/fixtures/*.html',
+                watched: true,
+                served: true,
+                included: false
+            }
         ]
     });
 };
---
+++ tests/spec/checkDOM.js
@@ -0,0 +1,26 @@
+/* jslint node: true */
+/* global describe, it, expect */
+
+"use strict";
+
+var fs       = require('fs');
+
+describe("DOM Checks", function() {
+
+    beforeEach(function() {
+        jasmine.getFixtures().fixturesPath = 'base/tests/fixtures';
+        loadFixtures('checkDOM.html');
+    });
+
+    it('checks if an element is in DOM', function() {
+        expect($('div')).toBeInDOM();
+    });
+
+    it('checks if an ID is in DOM', function() {
+        expect($('#test')).toBeInDOM();
+    });
+
+    it('checks if a class is in DOM', function() {
+        expect($('.test')).toBeInDOM();
+    });
+});
