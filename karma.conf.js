49bf62f8614a02d9608d8a8349d2732aaaa6743e
James Harris <wopiancitizen123@gmail.com>
2016-12-09 03:04:18 +0000

    Update

--- karma.conf.js
+++ karma.conf.js
@@ -4,10 +4,10 @@ module.exports = function(config) {
     frameworks:    ['browserify', 'jasmine-jquery', 'jasmine'],
     colors:        true,
     logLevel:      config.LOG_INFO,
-    reporters:     ['spec'],
+    reporters:     ['spec', 'coverage'],
     browsers:      ['PhantomJS'],
     preprocessors: {
-      'tests/**/*.js':     ['browserify'],
+      'tests/**/*.js':     ['browserify', 'coverage'],
       'tests/fixtures/js': ['babel']
     },
     browserify: {
@@ -15,13 +15,30 @@ module.exports = function(config) {
       // transform: ['brfs']
       transform: ['babelify']
     },
+    coverageReporter: {
+      // specify a common output directory
+      dir:       'build/reports/coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
+    },
     plugins: [
       'karma-jasmine',
       'karma-jasmine-jquery',
       'karma-phantomjs-launcher',
       'karma-spec-reporter',
       'karma-babel-preprocessor',
-      'karma-browserify'
+      'karma-browserify',
+      'karma-coverage'
       // 'brfs',
     ],
     files: [
