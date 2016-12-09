9128e1abc39a87d89d42cb6b799dcab74a66360a
James Harris <wopiancitizen123@gmail.com>
2016-12-09 03:19:04 +0000

    Test 3

--- karma.conf.js
+++ karma.conf.js
@@ -17,20 +17,12 @@ module.exports = function(config) {
       transform: ['babelify']
     },
     coverageReporter: {
+      type:                'lcovonly',
       // specify a common output directory
-      dir:       'coverage',
-      reporters: [
-        // reporters not supporting the `file` property
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' },
-        // reporters supporting the `file` property, use `subdir` to directly
-        // output them in the `dir` directory
-        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
-        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
-        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
-        { type: 'text', subdir: '.', file: 'text.txt' },
-        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
-      ]
+      dir:                 'coverage',
+      instrumenterOptions: {
+        istanbul: { noCompact: true }
+      }
     },
     plugins: [
       'karma-jasmine',
